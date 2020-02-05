e8a0bad19d7543d39cfc84d1853cc97359334694
Ryusk22 <kuppo0211@docomo.ne.jp>
2020-02-05 15:02:30 +0900

    ログイン画面表示

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
-    root 'bocabulary#index'
+  root 'bocabulary#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users, :controllers => {
+    :registrations => 'users/registrations',
+    :sessions => 'users/sessions'   
+  } 
+  devise_scope :user do
+    get "sign_in", :to => "users/sessions#new"
+  end
+
 end
