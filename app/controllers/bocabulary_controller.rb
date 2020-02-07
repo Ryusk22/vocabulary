f9670b642805ce6f0899641490d6aea5ff3c1167
Ryusk22 <kuppo0211@docomo.ne.jp>
2020-02-07 10:18:57 +0900

    単語メンテナンス画面と単語帳を作成

--- app/controllers/bocabulary_controller.rb
+++ app/controllers/bocabulary_controller.rb
@@ -1,16 +1,37 @@
 class BocabularyController < ApplicationController
   before_action :authenticate_user!
 
+  def new
+  end
+
   def index
   end
 
+  def show
+    @question = Question.new
+    @questions = Question.all
+    @question_similars = QuestionSimilar.all
+  end
+
   def create
+    Question.create(create_params)
+    redirect_to "/bocabulary/:#{current_user.id}"
   end
 
   def edit
+    @question = Question.find(params[:id])
   end
 
-  def delete
+  def destroy
+    question = Question.find(params[:id])
+    question.destroy
+    redirect_to "/bocabulary/:#{current_user.id}"
+  end
+
+  private
+
+  def create_params
+    params.require(:question).permit(:question, :discription)
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_04_072706) do
+ActiveRecord::Schema.define(version: 2020_02_05_090738) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "question_similars", force: :cascade do |t|
+    t.integer "question_id"
+    t.string "similar_word"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "questions", force: :cascade do |t|
+    t.string "question"
+    t.text "discription"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name", default: "", null: false
